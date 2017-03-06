c1218d85ddebc3ce19a3d66389afe9644f63678c
ketu.lai <ketu.lai@gmail.com>
2017-03-06 16:32:52 +0800

    add tests

--- src/Veda/Lazada/ResponseFactory.php
+++ src/Veda/Lazada/ResponseFactory.php
@@ -7,9 +7,8 @@
 namespace Veda\Lazada;
 
 use GuzzleHttp\Psr7\Response;
-use Veda\Lazada\Response\JsonResponse;
+use Veda\Lazada\Exception\InvalidArgumentException;
 use Veda\Lazada\Response\ResponseAbstract;
-use Veda\Lazada\Response\XmlResponse;
 
 class ResponseFactory
 {
@@ -20,11 +19,11 @@ class ResponseFactory
     public static function create($handler, Response $response)
     {
         if (!class_exists($handler)) {
-            throw new \InvalidArgumentException(sprintf("%s not exists", $handler));
+            throw new InvalidArgumentException("Response handler (%s) not exists", $handler);
         }
         $cls = new $handler($response);
         if (!$cls instanceof ResponseAbstract) {
-            throw new \InvalidArgumentException(sprintf("%s is not instanceof ResponseAbstract", $handler));
+            throw new InvalidArgumentException("%s is not instanceof ResponseAbstract", $handler);
         }
         return $cls;
     }
---
+++ tests/Utils/ArrayToXmlTest.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * User: ketu.lai <ketu.lai@gmail.com>
+ * Date: 2017/3/6
+ */
+
+namespace Veda\Lazada\Tests\Utils;
+
+use PHPUnit\Framework\TestCase;
+use Veda\Lazada\Utils\ArrayToXml;
+
+class ArrayToXmlTest extends TestCase
+{
+    public function testConvert()
+    {
+        $data = [
+            'user'=> 'xx',
+            'password'=> 'xxx',
+            'attributes'=> [
+                'gender'=> 'kl',
+                'age'=> '12',
+                'school'=> [
+                    'name'=> 'name',
+                    'value'=> 'XXX School',
+                    'attributes'=> [
+                        'address'=> 'CA',
+                        'area'=> 100
+                    ]
+                ],
+                'contacts'=>
+                [
+                    'father'=> '1234',
+                    'monther'=> '45678'
+                ]
+            ]
+        ];
+
+        $xml = ArrayToXml::convert($data);
+
+        $xmlHardCode = "<?xml version=\"1.0\"?>\n<root><user>xx</user><password>xxx</password><attributes><gender>kl</gender><age>12</age><school><name address=\"CA\" area=\"100\">XXX School</name></school><contacts><father>1234</father><monther>45678</monther></contacts></attributes></root>\n";
+
+        $this->assertEquals($xmlHardCode, $xml);
+
+    }
+}
